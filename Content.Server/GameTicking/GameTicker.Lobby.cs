9c55a917abbbe2d7aaab73ab7da74076895bbe96
G-man <kosuhinmatvej748@gmail.com>
2024-10-02 16:31:15 +0500

    u

--- Content.Server/GameTicking/GameTicker.Lobby.cs
+++ Content.Server/GameTicking/GameTicker.Lobby.cs
@@ -72,8 +72,6 @@ namespace Content.Server.GameTicking
                                     Loc.GetString("game-ticker-no-map-selected"));
             }
 
-            var planetName = _distressSignal.SelectedPlanetMapName ??
-                Loc.GetString("game-ticker-no-map-selected");
             var gmTitle = Loc.GetString(preset.ModeTitle);
             var desc = Loc.GetString(preset.Description);
             return Loc.GetString(
@@ -83,7 +81,6 @@ namespace Content.Server.GameTicking
                 ("roundId", RoundId),
                 ("playerCount", playerCount),
                 ("readyCount", readyCount),
-                ("planetName", planetName),
                 ("mapName", stationNames.ToString()),
                 ("gmTitle", gmTitle),
                 ("desc", desc));
@@ -153,6 +150,9 @@ namespace Content.Server.GameTicking
 
         public void ToggleReadyAll(bool ready)
         {
+            if(RunLevel != GameRunLevel.PreRoundLobby){
+                return;
+            }
             var status = ready ? PlayerGameStatus.ReadyToPlay : PlayerGameStatus.NotReadyToPlay;
             foreach (var playerUserId in _playerGameStatuses.Keys)
             {
@@ -165,6 +165,10 @@ namespace Content.Server.GameTicking
 
         public void ToggleReady(ICommonSession player, bool ready)
         {
+            if(RunLevel != GameRunLevel.PreRoundLobby){
+                return;
+            }
+
             if (!_playerGameStatuses.ContainsKey(player.UserId))
                 return;
 
@@ -187,6 +191,6 @@ namespace Content.Server.GameTicking
             => UserHasJoinedGame(session.UserId);
 
         public bool UserHasJoinedGame(NetUserId userId)
-            => PlayerGameStatuses[userId] == PlayerGameStatus.JoinedGame;
+            => PlayerGameStatuses.ContainsKey(userId) && PlayerGameStatuses[userId] == PlayerGameStatus.JoinedGame;
     }
 }
